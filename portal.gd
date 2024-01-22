c56cc92871c424401effa7ee85ab4bd52391c3ca
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2024-01-22 15:08:16 +0100

    Add spikes

--- portal.gd
+++ portal.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 var portalExit
+var portalSwitch
+@export var disabled = false
 
 func _ready():
-	portalExit = self.get_node("PortalExit")
+	$PortalEntry/CollisionShape2D.disabled = disabled
+	portalExit = $PortalExit
+	portalSwitch = $PortalSwitch
 
-func _onBodyEntered(body:Node2D):
+func _onPortalBodyEntered(body:Node2D):
 	body.global_position.x = portalExit.global_position.x
 	body.global_position.y = portalExit.global_position.y
+
+func _onSwitchBodyEntered(body:Node2D):
+	$PortalEntry/CollisionShape2D.set_deferred("disabled", false)
