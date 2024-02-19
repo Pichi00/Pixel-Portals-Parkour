a3d513a4e5c63e503f5fa59e2c30dfe93d8f825d
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2024-02-19 13:04:19 +0100

    Add platform collapse animation

--- collapsable_platform.gd
+++ collapsable_platform.gd
@@ -6,19 +6,23 @@ extends StaticBody2D
 func _ready():
 	$Timer.wait_time = initialWaitTime
 	$RespawnTimer.wait_time = initialRespawnTime
+	$CollapseSound.volume_db = Global.sfx
 
 func _on_timer_timeout():
-	$CollisionShape2D.disabled = true
-	$Area2D/CollisionShape2D.disabled = true
-	$Icon.visible = false
+#	$CollisionShape2D.disabled = true
+#	$Area2D/CollisionShape2D.disabled = true
+#	$Icon.visible = false
 	$RespawnTimer.start()
 
 
 func _on_regenerate_timer_timeout():
+	$AnimationPlayer.play("RESET")
 	$CollisionShape2D.disabled = false
 	$Area2D/CollisionShape2D.disabled = false
 	$Icon.visible = true
+	$AnimationPlayer.play("respawn")
 
 
 func _on_area_2d_body_entered(body):
+	$AnimationPlayer.play("break")
 	$Timer.start()
